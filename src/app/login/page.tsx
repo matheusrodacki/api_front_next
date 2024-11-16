2ca8b0fb3bb5dd7549e6d00f64d03f6d91aa07a0
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-11-16 17:36:34 -0300

    Atualiza estilos globais, adiciona página de login e configura Tailwind CSS com suporte a formulários

---
+++ src/app/login/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import LoginPage from '../../components/LoginPage/loginPage';
+
+export default function Login() {
+  return <LoginPage />;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
 } satisfies Config;
